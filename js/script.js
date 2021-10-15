8a7c924937e87db3392c40ddcabf18bb00199cd8
HarshitCoder437 <70216081+HarshitCoder437@users.noreply.github.com>
2021-10-15 14:34:36 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function whale() {
+    document.getElementById('whale-mp3').play();
+}
+
+function whaleOut() {
+    document.getElementById('whale-mp3').pause();
+    document.getElementById('whale-mp3').currentTime = 0;
+}
+
+function tiger() {
+    document.getElementById('tiger-mp3').play();
+}
+
+function tigerOut() {
+    document.getElementById('tiger-mp3').pause();
+    document.getElementById('tiger-mp3').currentTime = 0;
+}
+
+function lion() {
+    document.getElementById('lion-wav').play();
+}
+
+function lionOut() {
+    document.getElementById('lion-wav').pause();
+    document.getElementById('lion-wav').currentTime = 0;
+}
+
+function peacock() {
+    document.getElementById('peacock-wav').play();
+}
+
+function peacockOut() {
+    document.getElementById('peacock-wav').pause();
+    document.getElementById('peacock-wav').currentTime = 0;
+}
